11870c747916a04caf0ca03261f431fc46420005
gaprop <anders.kildemand@gmail.com>
2023-01-27 17:49:37 +0100

    Refactored some things

--- C/main.c
+++ C/main.c
@@ -1,4 +1,7 @@
 #include <ncurses.h>
+#include <stdlib.h>
+
+#define SIZE 3
 
 enum choice {
   X = 'X',
@@ -11,40 +14,137 @@ typedef struct Cursors {
   int y;
 } Cursor;
 
-enum choice cells[3][3] = {Nothing, Nothing, Nothing,
-                           Nothing, Nothing, Nothing,
-                           Nothing, Nothing, Nothing};
+enum side {
+  Left,
+  Right,
+};
 
-WINDOW *board[3][3];
+void init_ncurse();
+Cursor init_board(WINDOW *board[SIZE][SIZE], int max_y, int max_x);
+void render_board(Cursor cursor, WINDOW *board[SIZE][SIZE], enum choice cells[SIZE][SIZE]);
+void set_cell(int row, int col, enum choice choice, enum choice cells[SIZE][SIZE]);
+enum choice get_winner(enum choice cells[SIZE][SIZE]);
 
-Cursor init_board(WINDOW *board[3][3], int max_y, int max_x);
-void render_board(Cursor cursor, WINDOW *board[3][3]);
-void set_cell(int row, int col, enum choice choice);
-enum choice get_winner(void);
-
-int main() {
-
+void init_ncurse() {
   initscr();
   clear();
   noecho();
   cbreak();
   curs_set(0);
   keypad(stdscr, TRUE);
+  refresh();
+}
+
+Cursor init_board(WINDOW *board[SIZE][SIZE], int max_y, int max_x) {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j ++) {
+      /* WINDOW* cell = board[i][j]; */
+
+      board[i][j] = newwin(3, 5, (max_y / 2) + (i * 3), (max_x / 2) + (j * 5));
+      box(board[i][j], 0, 0);
+
+      // FIXME: Do not think these two lines are necessary
+      /* mvwaddch(board[i][j], 1, 2, cells[i][j]); */
+      /* wrefresh(board[i][j]); */
+    }
+  }
+
+  Cursor cursor;
+  cursor.x = 0;
+  cursor.y = 0;
+  return cursor;
+}
+
+void render_board(Cursor cursor, WINDOW *board[SIZE][SIZE], enum choice cells[SIZE][SIZE]) {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j ++) {
+      if ((i == cursor.y) && (j == cursor.x)) 
+        wattron(board[i][j], A_REVERSE);
+
+      mvwaddch(board[i][j], 1, 2, cells[i][j]);
+      wrefresh(board[i][j]);
+
+      wattroff(board[i][j], A_REVERSE);
+
+      wrefresh(board[i][j]);
+    }
+  }
+}
+
+void set_cell(int row, int col, enum choice choice, enum choice cells[SIZE][SIZE]) {
+  cells[row][col] = choice;
+}
+
+// Make better
+enum choice get_winner(enum choice cells[SIZE][SIZE]) {
+
+  // First check the rows and see if they are all the same choice
+  int O_row_eq = 0;
+  int O_col_eq = 0;
+
+  int X_row_eq = 0;
+  int X_col_eq = 0;
+
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      if (cells[i][j] == O) O_row_eq++;
+      if (cells[j][i] == O) O_col_eq++;
+      if (cells[i][j] == X) X_row_eq++;
+      if (cells[j][i] == X) X_col_eq++;
+    }
+
+    if (O_row_eq == SIZE) return O;
+    if (O_col_eq == SIZE) return O;
+    if (X_row_eq == SIZE) return X;
+    if (X_col_eq == SIZE) return X;
+
+    O_row_eq = 0;
+    O_col_eq = 0;
+    X_row_eq = 0;
+    X_col_eq = 0;
+  }
+
+  // Afterwards check across the board and see if they are equal
+  int O_lcross_eq = 0;
+  int O_rcross_eq = 0;
+  int X_lcross_eq = 0;
+  int X_rcross_eq = 0;
+
+  for (int i = 0; i < SIZE; i++) {
+    if (cells[i][i] == O) O_lcross_eq++;
+    if (cells[i][2 - i] == O) O_rcross_eq++;
+    if (cells[i][i] == X) X_lcross_eq++;
+    if (cells[i][2 - i] == X) X_rcross_eq++;
+  }
+
+  if (O_lcross_eq == SIZE) return O;
+  if (O_rcross_eq == SIZE) return O;
+  if (X_lcross_eq == SIZE) return X;
+  if (X_rcross_eq == SIZE) return X;
+
+  // In any other case return Nothing
+  return Nothing;
+}
+
+int main() {
 
   int max_x = 0;
   int max_y = 0;
   int c;
+  WINDOW *board[SIZE][SIZE];
   enum choice winner = Nothing;
   enum choice player = X;
+  enum choice cells[SIZE][SIZE] = {Nothing, Nothing, Nothing,
+                             Nothing, Nothing, Nothing,
+                             Nothing, Nothing, Nothing};
 
+  init_ncurse();
   getmaxyx(stdscr, max_y, max_x);
-
-  refresh();
 
   Cursor cursor = init_board(board, max_y, max_x);
 
   while (winner == Nothing) {
-    render_board(cursor, board);
+    render_board(cursor, board, cells);
 
     mvprintw(max_y / 2 + 3 * 4, max_x / 2, "It is %c's turn", player);
 
@@ -82,107 +182,16 @@ int main() {
         break;
 
       case ' ':
-        set_cell(cursor.y, cursor.x, player);
+        set_cell(cursor.y, cursor.x, player, cells);
         player = (player == X) ? O : X;
         break;
     }
-    winner = get_winner();
+    winner = get_winner(cells);
   }
   // We have to render the board one last time in case there is a winner, because otherwise the last choice will not be there.
-  render_board(cursor, board);
+  render_board(cursor, board, cells);
   mvprintw(max_y / 2 + 3 * 4, max_x / 2, "The winner is: %c", winner);
   mvprintw(max_y / 2 + 3 * 4 + 1, max_x / 2, "Press any key to quit");
   getch();
   endwin();
-}
-
-void render_board(Cursor cursor, WINDOW *board[3][3]) {
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j ++) {
-      if ((i == cursor.y) && (j == cursor.x)) 
-        wattron(board[i][j], A_REVERSE);
-
-      mvwaddch(board[i][j], 1, 2, cells[i][j]);
-      wrefresh(board[i][j]);
-
-      wattroff(board[i][j], A_REVERSE);
-
-      wrefresh(board[i][j]);
-    }
-  }
-}
-
-Cursor init_board(WINDOW *board[3][3], int max_y, int max_x) {
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j ++) {
-      /* WINDOW* cell = board[i][j]; */
-
-      board[i][j] = newwin(3, 5, (max_y / 2) + (i * 3), (max_x / 2) + (j * 5));
-      box(board[i][j], 0, 0);
-
-      // FIXME: Do not think these two lines are necessary
-      mvwaddch(board[i][j], 1, 2, cells[i][j]);
-      wrefresh(board[i][j]);
-    }
-  }
-
-  Cursor cursor;
-  cursor.x = 0;
-  cursor.y = 0;
-  return cursor;
-}
-
-// Make better
-enum choice get_winner() {
-
-  // First check the rows and see if they are all the same choice
-  int O_row_eq = 0;
-  int O_col_eq = 0;
-
-  int X_row_eq = 0;
-  int X_col_eq = 0;
-
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      if (cells[i][j] == O) O_row_eq++;
-      if (cells[j][i] == O) O_col_eq++;
-      if (cells[i][j] == X) X_row_eq++;
-      if (cells[j][i] == X) X_col_eq++;
-    }
-
-    if (O_row_eq == 3) return O;
-    if (O_col_eq == 3) return O;
-    if (X_row_eq == 3) return X;
-    if (X_col_eq == 3) return X;
-
-    O_row_eq = 0;
-    O_col_eq = 0;
-    X_row_eq = 0;
-    X_col_eq = 0;
-  }
-
-  // Afterwards check across the board and see if they are equal
-  int O_lcross_eq = 0;
-  int O_rcross_eq = 0;
-  int X_lcross_eq = 0;
-  int X_rcross_eq = 0;
-
-  for (int i = 0; i < 3; i++) {
-    if (cells[i][i] == O) O_lcross_eq++;
-    if (cells[i][2 - i] == O) O_rcross_eq++;
-    if (cells[i][i] == X) X_lcross_eq++;
-    if (cells[i][2 - i] == X) X_rcross_eq++;
-  }
-
-  if (O_lcross_eq == 3) return O;
-  if (O_rcross_eq == 3) return O;
-  if (X_lcross_eq == 3) return X;
-  if (X_rcross_eq == 3) return X;
-
-  // In any other case return Nothing
-  return Nothing;
-}
-
-void set_cell(int row, int col, enum choice choice) {
-  cells[row][col] = choice;
 }
